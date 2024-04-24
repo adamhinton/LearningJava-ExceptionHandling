71a5967f5a9b04f6d31b625aa91f74b0f584bc39
adamd <adam.d.hinton@gmail.com>
2024-04-23 20:09:19 -0400

    Another way to handle IOException

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,20 +1,26 @@
 package dev.lpa;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         String filename = "testing.csv";
         Path path = Paths.get(filename);
-        try {
-            List<String> lines = Files.readAllLines(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+//        try {
+//            List<String> lines = Files.readAllLines(path);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        File file = new File(filename);
+        if (!file.exists()){
+            System.out.println("I can't run unless this file exists");
+            System.out.println("Quitting application, go figure it out");
+            // returning in main causes app to end
+            return;
         }
 
     }
