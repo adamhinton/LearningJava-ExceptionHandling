f53f278f4a051a8b6e51df7dea146d58a25490ac
adamd <adam.d.hinton@gmail.com>
2024-04-23 20:05:59 -0400

    my first IOException

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,21 @@
 package dev.lpa;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        String filename = "testing.csv";
+        Path path = Paths.get(filename);
+        try {
+            List<String> lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
