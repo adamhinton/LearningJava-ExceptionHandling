c8420252a1baeae09b1236e85ee9783ac70e8c75
adamd <adam.d.hinton@gmail.com>
2024-04-24 00:55:46 -0400

    exception types and multiple catch clauses

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,17 +1,17 @@
 package dev.lpa;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         String filename = "testing.csv";
-        testFile(filename);
+        testFile2(filename);
 
         File file = new File(filename);
         if (!file.exists()){
@@ -39,16 +39,48 @@ public class Main {
 
     private static void testFile(String filename) throws IOException {
         Path path = Paths.get(filename);
+        FileReader reader = null;
+
         try {
-            List<String> lines = Files.readAllLines(path);
+//            List<String> lines = Files.readAllLines(path);
+            reader = new FileReader(filename);
         }
         catch (IOException e) {
             throw new RuntimeException(e);
         }
         finally{
+            if(reader != null){
+                // Make sure to close file when done, usually in finally block
+                try{
+                    reader.close();
+                }
+                catch(IOException e){
+                    throw new RuntimeException(e);
+                }
+            }
             System.out.println("Maybe I'd log something either way");
         }
 
         System.out.println("File exists and able to use as a resource");
+    }
+
+    // more elegant way than original testFile
+    private static void testFile2(String fileName){
+        try (FileReader reader = new FileReader(fileName)) {
+            // Can have multiple catch clauses
+            // This might occur when opening or closing resource
+        } catch (FileNotFoundException e) {
+            System.out.println("File '" + fileName + " ' does not exist" );
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch(Exception e){
+            System.out.println("Something unrelated and unexpected happened");
+        }
+        finally{
+            System.out.println("Maybe I'd log something either way...");
+        }
+        System.out.println("File exists");
     }
 }
