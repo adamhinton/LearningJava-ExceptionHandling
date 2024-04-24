c49c880fd5d7073e6d8c5a13d13d8a5e2496c600
adamd <adam.d.hinton@gmail.com>
2024-04-23 20:10:57 -0400

    LBYL vs EAFP

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -22,6 +22,17 @@ public class Main {
             // returning in main causes app to end
             return;
         }
+        System.out.println("Good to go, file exists");
+
+        // TWO APPROACHES:
+
+        // LBYL
+        // Look before you leap
+        // check for errors before performing an op
+
+        // EAFP
+        // Easier to ask forgiveness than permission
+        // Assumes op will usually succeed, and handles any errors that occur if they do happen
 
     }
 }
