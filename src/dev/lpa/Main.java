25ef4c5e4555f7435b54cf31ad83c1dc442bfb65
adamd <adam.d.hinton@gmail.com>
2024-04-24 00:45:08 -0400

    init commit Tuesday evening

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,19 +1,17 @@
 package dev.lpa;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String filename = "testing.csv";
-        Path path = Paths.get(filename);
-//        try {
-//            List<String> lines = Files.readAllLines(path);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        testFile(filename);
 
         File file = new File(filename);
         if (!file.exists()){
@@ -34,5 +32,23 @@ public class Main {
         // Easier to ask forgiveness than permission
         // Assumes op will usually succeed, and handles any errors that occur if they do happen
 
+
+
+    }
+
+
+    private static void testFile(String filename) throws IOException {
+        Path path = Paths.get(filename);
+        try {
+            List<String> lines = Files.readAllLines(path);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally{
+            System.out.println("Maybe I'd log something either way");
+        }
+
+        System.out.println("File exists and able to use as a resource");
     }
 }
